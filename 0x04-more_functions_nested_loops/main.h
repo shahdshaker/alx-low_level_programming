d26ea4dfac808a0d62e766295affe363bfe1e993
shahdshaker <shahdshaker203@gmail.com>
2023-07-20 15:02:47 -0700

    C - More functions, more nested loops C

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -12,5 +12,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int size);
+void print_number(int n);
+
 #endif
+
