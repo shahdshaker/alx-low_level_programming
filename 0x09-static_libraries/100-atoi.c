dea92423ed23ceb2559a5a49e128ffa9fc0882c9
shahdshaker <shahdshaker203@gmail.com>
2023-08-04 17:28:34 -0700

    Update

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _atoi - convert a string into an integer.
  *
@@ -6,11 +7,11 @@
  *
  * Return: integer.
  */
+
 int _atoi(char *s)
 {
 int sign = 1, i = 0;
 unsigned int res = 0;
-
 
 while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 {
@@ -26,3 +27,4 @@ i++;
 res *= sign;
 return (res);
 }
+
